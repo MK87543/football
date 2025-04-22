bbd49aa2b7af6d33fe6c86875fc9476b8055b31d
MK87543 <134004067+MK87543@users.noreply.github.com>
2025-04-22 09:27:23 +0200

    React_football_03_added_api_call__improved_layout

--- src/components/League_card.tsx
+++ src/components/League_card.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
 
-type Props = {}
+type Props = {
+  Name: string;
+  Shortcut: string;
+  Season: string;
+  Sport: string;
+}
 
-export default function League_card({}: Props) {
+export default function League_card({ Name, Shortcut, Season, Sport }: Props) {
   return (
-    <div className='w-50 h-50 border'>
-        <div>Name</div>
-        <div>Land</div>
-        <div>Bild</div>
-
-
-
-
+    <div className='w-50 h-50 border p-3 m-2 hover:w-55 hover:h-55 hover:cursor-pointer hover:bg-gray-200 transition-all duration-300 ease-in-out'> 
+      <div className="font-bold">{Name}</div>
+      <div>Shortcut: {Shortcut}</div>
+      <div>Season: {Season}</div>
+      <div>Sport: {Sport}</div>
     </div>
-    
   )
 }
--- src/components/overview.tsx
+++ src/components/overview.tsx
@@ -1,27 +1,55 @@
-// File: src/components/overview.tsx
-
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import League_card from './League_card'
 
 type Props = {}
 
-export default function overview({}: Props) {
+interface League {
+  leagueId: number;
+  leagueName: string;
+  leagueShortcut: string;
+  leagueSeason: string;
+  sport: {
+    sportId: number;
+    sportName: string;
+  };
+}
+
+export default function Overview({}: Props) {
+  const [leagues, setLeagues] = useState<League[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetchAllLeagues();
+  }, []);
+
+  function fetchAllLeagues() {
+    fetch("https://api.openligadb.de/getavailableleagues")
+      .then(response => response.json())
+      .then(leaguesData => {
+        console.log("Leagues", leaguesData);
+        setLeagues(leaguesData);
+        setIsLoading(false);
+      })
+      .catch(error => {
+        console.error("Error fetching leagues:", error);
+        setIsLoading(false);
+      });
+  }
+
   return (
-    <div>overview
-
-        <div>Hier stehen alle Ligen in Karten form</div>
-        <div className='flex gap-2'>        
-        <League_card />
-        <League_card />
-        <League_card />
-        <League_card />
-        <League_card />
-        <League_card />
-        <League_card />
-        <League_card /></div>
-
-
+    <div>
+      <h2 className='text-xl font-bold mb-4'>Overview</h2>
+      <div>Hier stehen alle Ligen in Karten form</div>
+      <div className='flex flex-wrap gap-2 align-center justify-center'>
+          {leagues.map((league) => (
+            <League_card 
+              Name={league.leagueName}
+              Shortcut={league.leagueShortcut}
+              Season={league.leagueSeason}
+              Sport={league.sport?.sportName}
+            />
+          ))}
+        </div>
     </div>
-
   )
 }
